4fc57eca34743bbca5ac37bd35e5a352041d5556
toy_bear <toy_bear@outlook.com>
2020-04-19 11:16:19 +0800

    Backup Script

--- backup.sh
+++ backup.sh
@@ -1,9 +1,36 @@
 #!/bin/sh
 
 
-filelist=(rofi backgrounds i3 i3status alacritty ranger)
+filelist=(
+    ".pip" 
+    ".vim" 
+    ".icons" 
+    ".themes"
+    ".vimrc"                            
+    ".oh-my-zsh"                        
+    ".zshrc"                            
+    ".xprofile"                          
+    ".config/i3"                        
+    ".config/polybar"                      
+    ".config/i3status"                  
+    ".config/alacritty"                    
+    ".config/backgrounds"               
+    ".config/ranger"                    
+    ".config/rofi"                      
+    ".config/JetBrains"                 
+    ".config/user-dirs.dirs"            
+    ".config/user-dirs.locale"          
+)
 
-for i in "${filelist}"
+mkdir -p "archlinux/etc"
+mkdir -p "archlinux/home/toy/.config"
+
+for i in "${filelist[@]}"
 do
-    rsync -av "${HOME}/.config/${i}" "${HOME}/Workspace/dotfiles/config/${i}"
+    if [ -d "${HOME}/$i" ]
+    then
+        rsync -av "${HOME}/$i" "archlinux/home/toy/$i"
+    else
+        continue
+    fi
 done
